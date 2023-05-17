831aa994934cd6e8742590cffff7114ee1f7e525
lazar93kg <lazar93kg@gmail.com>
2023-05-17 16:30:19 +0200

    Update 1.0

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
-const withSvgr = require("next-svgr");
-
-
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: '@svgr/webpack'
+    })
+    return config
+  },
+  images: {
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "res.cloudinary.com",
+      port: "",
+    }, ],
+  },
 };
+
 module.exports = nextConfig;
-module.exports = withSvgr({
-  // your config for other plugins or the general next.js here...
-});
